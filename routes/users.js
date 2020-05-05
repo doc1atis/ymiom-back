2c659131efdc0650ea3eddec5de1cd9f4885eee8
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-05-05 17:50:10 -0400

    acc

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const login = require("../AUTH/login");
 // REFER TO THE PRE-CREATED BUCKET
 // TRY TO GET THE BUCKET INSTEAD AND LOOP OVER ALL BUCKET TO GET THE ONE YOU WANT. --> IF NOT EXIST,EC2 IS NOT HEALTHY
 const BUCKET_NAME = "miom-bucket";
+console.log(AWS.config.credentials);
 
 // CONFIGURE S3 AUTH
 const s3Auth = new AWS.S3({
