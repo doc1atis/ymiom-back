e46962a181510bc2d9f450cf9fd80cd64c5b9b31
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-05-06 18:05:24 -0400

    app

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.set("view engine", "jade");
 // USE CORS FOR HTT REQUEST
 // corsOptions
 // =================================================================================
-// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
@@ -48,9 +48,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // USE ALL APP ROUTERS, --> MUST START WITH /api
-app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
 // SERVE STATIC FILE
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
