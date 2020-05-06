f1770f0210eb4a29ab871ec0ec57eb01512ef58f
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-05-06 18:22:33 -0400

    refresh

--- app.js
+++ app.js
@@ -38,16 +38,15 @@ app.set("view engine", "jade");
 // USE CORS FOR HTT REQUEST
 // corsOptions
 // =================================================================================
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-
 // USE ALL APP ROUTERS, --> MUST START WITH /api
 app.use("/api/users", usersRouter);
+
 // SERVE STATIC FILE
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
@@ -55,6 +54,7 @@ app.get("/", (req, res) => {
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
